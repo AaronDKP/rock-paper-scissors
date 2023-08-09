f5c2b4493b2dd35cdeef028c3ac95f99a35bcf6e
AaronDKP <138345602+AaronDKP@users.noreply.github.com>
2023-08-10 08:48:18 +1000

    Alert output for initial game result
    Alert will display with win/loss/tie depending on both user and computer inputs to display result of initial game

--- javascript.js
+++ javascript.js
@@ -13,39 +13,74 @@
 
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-
-  }
-
-  
-  function getComputerChoice(){
-    let randomChoice = Math.floor(Math.random() * 3);
     
-    if(randomChoice === 0){
-        return("Rock!")
-    } else if(randomChoice === 1){
-        return("Paper!")
-    } else {
-        return("Scissors!")
+    // computer input
+
+    function getComputerChoice(){
+        let randomChoice = Math.floor(Math.random() * 3);
+        
+        if(randomChoice === 0){
+            return("Rock!")
+        } else if(randomChoice === 1){
+            return("Paper!")
+        } else {
+            return("Scissors!")
+        }
     }
+
+    // player input
+    
+    function getPlayerSelection(){
+        let query = prompt("Rock, Paper or Scissors?");
+            if(query.toLowerCase() === 'rock'){
+                return('Rock!')
+            } else if (query.toLowerCase() === 'paper'){
+                return('Paper!')
+            } else if (query.toLowerCase() === 'scissors'){
+                return('Scissors!')
+            } else {
+                alert('Please enter "Rock", "Paper" or "Scissors"')
+            }
+    
+        }
+    
+      playerSelection = getPlayerSelection();
+      computerSelection = getComputerChoice();
+
+
+    //output if player selection = rock
+
+    if (playerSelection === 'Rock!' && computerSelection === 'Scissors!'){
+        alert('You Win!');
+    } else if (playerSelection === 'Rock!' && computerSelection === 'Paper!'){
+        alert('You Lose!')
+    } else if(playerSelection === 'Rock!' && computerSelection === 'Rock!'){
+        alert("It's a Tie!")
+    }
+
+    //output if player selection = paper
+
+    if (playerSelection === 'Paper!' && computerSelection === 'Rock!'){
+        alert('You Win!');
+    } else if (playerSelection === 'Paper!' && computerSelection === 'Scissors!'){
+        alert('You Lose!')
+    } else if(playerSelection === 'Paper!' && computerSelection === 'Paper!'){
+        alert("It's a Tie!")
+    }
+
+        
+    //output if player selection = scissors
+
+    if (playerSelection === 'Scissors!' && computerSelection === 'Paper!'){
+        alert('You Win!');
+    } else if (playerSelection === 'Scissors!' && computerSelection === 'Rock!'){
+        alert('You Lose!')
+    } else if(playerSelection === 'Scissors!' && computerSelection === 'Scissors!'){
+        alert("It's a Tie!")
+    }
+
+
+
 }
 
-function getPlayerSelection(){
-    let query = prompt("Rock, Paper or Scissors?");
-        if(query.toLowerCase() === 'rock'){
-            return('Rock!')
-        } else if (query.toLowerCase() === 'paper'){
-            return('Paper!')
-        } else if (query.toLowerCase() === 'scissors'){
-            return('Scissors!')
-        } else {
-            return('Please enter "Rock", "Paper" or "Scissors"')
-        }
-
-    }
-
-    console.log(getPlayerSelection())
-
-  let playerSelection = getPlayerSelection();
-  let computerSelection = getComputerChoice();
-  //console.log(playRound(playerSelection, computerSelection));
+ console.log(playRound());
